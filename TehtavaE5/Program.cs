e07afe728ec0c7b95f0c964caf71cd4fdd02b755
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-08-25 08:53:31 +0300

    Add project files.

---
+++ TehtavaE5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PisinSana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Syötä lause: ");
+            string lause = Console.ReadLine();
+
+            string[] sanat = lause.Split();  // Jakaa lauseen sanoiksi erotettuna välilyönneillä
+
+            string pisinSana = "";  // Alustetaan pisimmän sanan muuttuja tyhjällä merkkijonolla
+
+            // Käydään läpi jokainen sana lauseesta
+            foreach (string sana in sanat)
+            {
+                if (sana.Length > pisinSana.Length)
+                {
+                    pisinSana = sana;  // Päivitetään pisimmän sanan muuttuja tarvittaessa
+                }
+            }
+
+            Console.WriteLine("Pisimmän sanan: " + pisinSana);
+        }
+    }
+}
+
